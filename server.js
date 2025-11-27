40a6f3014e0effeb70de43bc8b75b4908275acf4
josueps15 <josueportilla991@gmail.com>
2025-11-27 16:10:35 -0500

    Try port 587 for email to fix timeout

--- server.js
+++ server.js
@@ -54,8 +54,8 @@ pool.connect((err, client, release) => {
 // Email Configuration
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
